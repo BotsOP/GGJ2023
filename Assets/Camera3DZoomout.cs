7c2b8f907aa0783f39c54176d9b8e0e638586767
Poesbroek <yourischuring@gmail.com>
2023-02-04 00:40:51 +0100

    Swag

---
+++ Assets/Camera3DZoomout.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera3DZoomout : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ActivateScript()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/YouriFolder/Camera2DTilt.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera2DTilt : MonoBehaviour
+{
+    [SerializeField][Range(-1.0f, 1.0f)] private float rootXPos;
+    [SerializeField] private Animator KusjeKriebel;
+    [SerializeField] Vector3 turingPointPos;
+    [SerializeField] Vector3 turingPointRot;
+
+    public void Update()
+    {
+        transform.position = new Vector3 (turingPointPos.x * rootXPos,1 - turingPointPos.y * -Mathf.Abs(rootXPos) ,-10 - turingPointPos.z * -Mathf.Abs(rootXPos));   
+        transform.rotation = Quaternion.Euler(turingPointRot * rootXPos);   
+        if(rootXPos == 1) 
+        {
+
+            KusjeKriebel.SetBool("IsLinks", false);
+            ActivateAnimatorAndFallAsleep();
+
+        }
+        else if(-rootXPos == 1) 
+        {
+            KusjeKriebel.SetBool("IsLinks", true);
+            ActivateAnimatorAndFallAsleep();
+        }
+    }
+
+    public void ActivateAnimatorAndFallAsleep() 
+    {
+        KusjeKriebel.enabled = true;
+        this.enabled = false;
+    }
+}
