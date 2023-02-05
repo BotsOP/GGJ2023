9423c317c4448e6bd1bf032442b9906764442bc0
LAPTOP-IQ9F4R2Q\prutm <prutmaild@gmail.com>
2023-02-05 02:13:50 +0100

    added features for look at vine

--- Assets/LookAtVineRotation.cs
+++ Assets/LookAtVineRotation.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LookAtVine : MonoBehaviour
+public class LookAtVineRotation : MonoBehaviour
 {
     public VineManager vineManager;
     public float smoothSpeed;
